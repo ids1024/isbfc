1ae6991504cfdb24bd9f961686d1030356300d21
Ian D. Scott <ian@iandouglasscott.com>
2016-03-26 12:37:16 -0700

    Optimize MOVE + ADD + MOVE

--- bfcomp.py
+++ bfcomp.py
@@ -17,9 +17,9 @@ def parse(code):
     tokens = []
     for i in code:
         if i == '+':
-            tokens.append((ADD, 1))
+            tokens.append((ADD, (0, 1)))
         elif i == '-':
-            tokens.append((ADD, -1))
+            tokens.append((ADD, (0, -1)))
         elif i == '>':
             tokens.append((MOVE, 1))
         elif i == '<':
@@ -40,20 +40,21 @@ def optimize(tokens):
     newtokens = []
     for token, value in tokens:
         if add and token != ADD:
-            newtokens.append((ADD, add))
+            newtokens.append((ADD, (0, add)))
             add = 0
         elif move and token != MOVE:
             newtokens.append((MOVE, move))
             move = 0
 
         if token == ADD:
-            add += value
+            assert value[0] == 0
+            add += value[1]
         elif token == MOVE:
             move += value
         else:
             newtokens.append((token, value))
     if add:
-        newtokens.append((ADD, add))
+        newtokens.append((ADD, (0, add)))
     elif move:
         newtokens.append((MOVE, move))
 
@@ -77,24 +78,26 @@ def optimize(tokens):
                 value = 0
                 # ADD after SET can be simplified to SET
                 if i<len(newtokens) and newtokens[i][0] == ADD:
-                    value = newtokens[i][1]
+                    assert newtokens[i][1][0] == 0
+                    value = newtokens[i][1][1]
                     del newtokens[i]
                 newtokens.insert(i, (SET, (0, value)))
         i += 1
 
-    # Optimize MOVE + SET + MOVE
+    # Optimize MOVE + SET + MOVE and MOVE + ADD + MOVE
     i = 0
     while i < len(newtokens)-2:
         if (newtokens[i][0] == MOVE and
-             newtokens[i+1][0] == SET and
+             newtokens[i+1][0] in (SET, ADD) and
              newtokens[i+2][0] == MOVE):
 
+            opp = newtokens[i+1][0] # SET or ADD
             assert newtokens[i+1][1][0] == 0
             value = newtokens[i+1][1][1]
             offset = newtokens[i][1]
             move = offset + newtokens[i+2][1]
             del newtokens[i:i+3]
-            newtokens.insert(i, (SET, (offset, value)))
+            newtokens.insert(i, (opp, (offset, value)))
             newtokens.insert(i+1, (MOVE, move))
 
         i += 1
@@ -117,14 +120,19 @@ _start:
     loopnum = 0
     for token, value in tokens:
         if token == ADD:
-            if value == 1:
-                output += "    inc %r12\n"
-            elif value > 1:
-                output += "    add $" + str(value) + ", %r12\n"
-            elif value == -1:
-                output += "    dec %r12\n"
-            elif value < -1:
-                output += "    sub $" + str(-value) + ", %r12\n"
+            offset, value = value
+            if offset == 0:
+                dest = "%r12"
+            else:
+                dest = str(offset*8)+"(%rbx)"
+            if value == 1 and dest == "%r12":
+                output += "    inc " + dest + "\n"
+            elif value >= 1:
+                output += "    add $" + str(value) + ", " + dest + "\n"
+            elif value == -1 and dest == "%r12":
+                output += "    dec " + dest + "\n"
+            elif value <= -1:
+                output += "    sub $" + str(-value) + ", " + dest + "\n"
         elif token == SET:
                 offset, value = value
                 if offset == 0:
