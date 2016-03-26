39334e319dcf2e938d6f1a4dc575db6eb068a639
Ian D. Scott <ian@iandouglasscott.com>
2016-03-26 15:43:32 -0700

    Move parser out to file; partially port interpreter to use it

--- bfinterp.py
+++ bfinterp.py
@@ -3,40 +3,60 @@ import collections
 
 import getch
 
+from parser import parse, optimize
+from parser import OUTPUT, INPUT, LOOPSTART, LOOPEND, MOVE
+from parser import ADD, SET, MULCOPY, SCAN
+
+BUFSIZE = 8192
+
 def interp(code):
-    code = ''.join(i for i in code if i in '.,[]+-<>')
+    tokens = parse(code)
+    tokens = optimize(tokens)
     i = 0
     loops = []
-    mem = bytearray(1024)
-    cur = 512
-    while i < len(code)-1:
+    mem = bytearray(BUFSIZE)
+    cur = int(BUFSIZE/2)
+    while i < len(tokens)-1:
         #print("%d:%s cur:%d mem[cur]:%d" % (i, code[i], cur, mem[cur]))
         #print(loops)
-        if code[i] == '.':
+        token, value = tokens[i]
+        if token == OUTPUT:
             print(chr(mem[cur]), end='')
-        elif code[i] == ',':
+        elif token == INPUT:
             mem[cur] == ord(getch.getch())
-        elif code[i] == '<':
-            cur -= 1
-        elif code[i] == '>':
-            cur += 1
-        elif code[i] == '+':
-            if mem[cur] == 255:
-                cur = 0
-            else:
-                mem[cur] += 1
-        elif code[i] == '-':
-            if mem[cur] == 0:
-                mem[cur] = 255
-            else:
-                mem[cur] -= 1
-        elif code[i] == '[':
+        elif token == MOVE:
+            cur += value
+        elif token == ADD:
+            offset, add = value
+            newval = mem[cur+offset] + add
+            while newval < 0:
+                newval += 256
+            while newval > 255:
+                newval -= 256
+            mem[cur+offset] = newval
+        elif token == SET:
+            offset, val = value
+            mem[cur+offset] = val
+        elif token == MULCOPY:
+            src, dest, mul = value
+            newval = mem[cur+dest] + mem[cur+src] * mul
+            while newval < 0:
+                newval += 256
+            while newval > 255:
+                newval -= 256
+            mem[dest+offset] = newval
+        elif token == SCAN:
+            while mem[cur] != 0:
+                cur += value
+        elif token == LOOPSTART:
             loops.append(i)
-        elif code[i] == ']':
+        elif token == LOOPEND:
             if mem[cur]:
                 i = loops[-1]
             else:
                 loops.pop()
+        else:
+            raise ValueError('Token not handled')
 
         i += 1
 
---
+++ parser.py
@@ -0,0 +1,136 @@
+OUTPUT=0
+INPUT=1
+LOOPSTART=2
+LOOPEND=3
+MOVE=4
+ADD=5
+SET=6
+MULCOPY=7
+SCAN=8
+
+def parse(code):
+    code = ''.join(i for i in code if i in '.,[]+-<>')
+    tokens = []
+    for i in code:
+        if i == '+':
+            tokens.append((ADD, (0, 1)))
+        elif i == '-':
+            tokens.append((ADD, (0, -1)))
+        elif i == '>':
+            tokens.append((MOVE, 1))
+        elif i == '<':
+            tokens.append((MOVE, -1))
+        elif i == '[':
+            tokens.append((LOOPSTART, None))
+        elif i == ']':
+            tokens.append((LOOPEND, None))
+        elif i == ',':
+            tokens.append((INPUT, None))
+        if i == '.':
+            tokens.append((OUTPUT, None))
+    return tokens
+
+def optimize(tokens):
+    add = 0
+    move = 0
+    newtokens = []
+    for token, value in tokens:
+        if add and token != ADD:
+            newtokens.append((ADD, (0, add)))
+            add = 0
+        elif move and token != MOVE:
+            newtokens.append((MOVE, move))
+            move = 0
+
+        if token == ADD:
+            assert value[0] == 0
+            add += value[1]
+        elif token == MOVE:
+            move += value
+        else:
+            newtokens.append((token, value))
+    if add:
+        newtokens.append((ADD, (0, add)))
+    elif move:
+        newtokens.append((MOVE, move))
+
+    # Optimize out clear loop
+    i = 0
+    while i < len(newtokens):
+        if newtokens[i][0] == LOOPSTART:
+            j = i + 1
+            while j < len(newtokens) and newtokens[j][0] != LOOPEND:
+                if newtokens[j][0] != ADD:
+                    break
+                j += 1
+            else:
+                del newtokens[i:j+1]
+                # ADD before clear does nothing, so remove it
+                if i>0 and newtokens[i-1][0] == ADD:
+                    del newtokens[i-1]
+                    i -= 1
+                value = 0
+                # ADD after SET can be simplified to SET
+                if i<len(newtokens) and newtokens[i][0] == ADD:
+                    assert newtokens[i][1][0] == 0
+                    value = newtokens[i][1][1]
+                    del newtokens[i]
+                newtokens.insert(i, (SET, (0, value)))
+        i += 1
+
+    # Optimize MOVE + SET + MOVE and MOVE + ADD + MOVE
+    i = 0
+    while i < len(newtokens)-2:
+        if (newtokens[i][0] == MOVE and
+             newtokens[i+1][0] in (SET, ADD) and
+             newtokens[i+2][0] == MOVE):
+
+            opp = newtokens[i+1][0] # SET or ADD
+            assert newtokens[i+1][1][0] == 0
+            value = newtokens[i+1][1][1]
+            offset = newtokens[i][1]
+            move = offset + newtokens[i+2][1]
+            del newtokens[i:i+3]
+            newtokens.insert(i, (opp, (offset, value)))
+            if move:
+                newtokens.insert(i+1, (MOVE, move))
+
+        i += 1
+
+    # Optimize copy/multiplication
+    i = 0
+    while i < len(newtokens):
+        if newtokens[i][0] == LOOPSTART:
+            j = i + 1
+            adds = {}
+            while j < len(newtokens) and newtokens[j][0] != LOOPEND:
+                if newtokens[j][0] != ADD:
+                    break
+                offset, add = newtokens[j][1]
+                adds[offset] = adds.get(offset, 0) + add
+                j += 1
+            else:
+                if 0 not in adds:
+                    print("Warning: Infinite loop detected.")
+                elif adds[0] == -1:
+                    del adds[0]
+                    del newtokens[i:j+1]
+                    for k, v in adds.items():
+                        newtokens.insert(i, (MULCOPY, (0, k, v)))
+                        i += 1
+                    newtokens.insert(i, (SET, (0, 0)))
+        i += 1
+
+    # Optimize scan loop
+    i = 0
+    while i < len(newtokens)-2:
+        if (newtokens[i][0] == LOOPSTART and
+             newtokens[i+1][0] == MOVE and
+             newtokens[i+2][0] == LOOPEND):
+
+            offset = newtokens[i+1][1]
+            del newtokens[i:i+3]
+            newtokens.insert(i, (SCAN, offset))
+        i += 1
+
+    return newtokens
