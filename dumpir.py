047a4dda18be39e3283459a2558822ef86b8e374
Ian D. Scott <ian@iandouglasscott.com>
2016-03-27 08:40:12 -0700

    Add dumpir.py for debugging

---
+++ dumpir.py
@@ -0,0 +1,35 @@
+import sys
+
+from parser import parse, optimize
+from parser import OUTPUT, INPUT, LOOPSTART, LOOPEND, MOVE
+from parser import ADD, SET, MULCOPY, SCAN
+
+
+def dumpir(code):
+    tokens = parse(code)    
+    tokens = optimize(tokens)
+    for token, value in tokens:
+        if token == OUTPUT:
+            print('output')
+        elif token == INPUT:
+            print('input')
+        elif token == LOOPSTART:
+            print('loopstart')
+        elif token == LOOPEND:
+            print('loopend')
+        elif token == MOVE:
+            print('move(offset=%d) % value')
+        elif token == ADD:
+            print('add(offset=%d, value=%d)' % value)
+        elif token == SET:
+            print('set(offset=%d, value=%d)' % value)
+        elif token == MULCOPY:
+            print('mulcopy(src=%d, dest=%d, mul=%d)' % value)
+        elif token == SCAN:
+            print('scan(offset=%d)' % value)
+        else:
+            print('TOKEN', i, 'NOT HANDLED')
+
+if __name__ == '__main__':
+    with open(sys.argv[1]) as bffile:
+        dumpir(bffile.read())
