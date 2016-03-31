56e5f26d751719b38c018230619c2b5eb754e61c
Ian D. Scott <ian@iandouglasscott.com>
2016-03-31 11:44:35 -0700

    Finally eliminate old optimizer loop

--- parser.py
+++ parser.py
@@ -46,7 +46,9 @@ def optimize(tokens):
     adds = collections.OrderedDict()
     sets = collections.OrderedDict()
     do_output = False
+    pre_loop_sets = collections.OrderedDict()
     for token, value in tokens:
+        prev_token = newtokens[-1][0] if newtokens else None
         # Optimize out clear loop / multiply move loop
         if (token == ENDLOOP and newtokens[-1][0] == LOOP and
                 not shift and 0 in adds):
@@ -56,11 +58,13 @@ def optimize(tokens):
                     newtokens.append((IF, 0))
                     for offset, val in sets.items():
                         newtokens.append((SET, (offset, val)))
-                newtokens.append((SET, (0, 0)))
-                if sets:
+                    sets.clear()
+                    newtokens.append((SET, (0, 0)))
                     newtokens.append((ENDIF, None))
+                else:
+                    sets[0] = 0
+                pre_loop_sets.clear()
                 adds.clear()
-                sets.clear()
                 continue
             elif adds[0] == -1:
                 newtokens.pop() # Remove LOOP
@@ -70,13 +74,25 @@ def optimize(tokens):
                         newtokens.append((SET, (k, v)))
                 for k, v in adds.items():
                     if k != 0:
-                        newtokens.append((MULCOPY, (0, k, v)))
+                        src, dest, mul = 0, k, v
+                        if src in pre_loop_sets:
+                            val = pre_loop_sets[src] * mul
+                            newtokens.append((ADD, (dest, val)))
+                        else:
+                            newtokens.append((MULCOPY, (src, dest, mul)))
                 if sets:
                     newtokens.append((ENDIF, None))
-                newtokens.append((SET, (0, 0)))
+                pre_loop_sets.clear()
                 adds.clear()
                 sets.clear()
+                sets[0] = 0
                 continue
+
+        if token == LOOP:
+            for k, v in sets.items():
+                pre_loop_sets[k+shift] = v
+        elif token not in (SET, ADD, MOVE):
+            pre_loop_sets.clear()
 
         if token not in (SET, ADD, MOVE, LOADOUT, LOADOUTSET, OUTPUT):
             if do_output:
@@ -110,7 +126,7 @@ def optimize(tokens):
         elif token == MULCOPY:
             src, dest, mul = value
             newtokens.append((MULCOPY, (src+shift, dest+shift, mul)))
-        # XXX Deal with shift in if
+        # XXX Deal with shift in if, if those are ever generated
         elif token == IF:
             offset = shift + value
             newtokens.append((IF, offset))
@@ -145,39 +161,8 @@ def optimize(tokens):
     if do_output:
         newtokens.append((OUTPUT, None))
 
-    newtokens2 = []
-
-    i = 0
-    while i < len(newtokens):
-        optimized = False
-
-        # SET + MULCOPY = SET + ADD
-        if (not optimized and
-             i < len(newtokens)-1 and
-             newtokens[i][0] == SET and
-             newtokens[i+1][0] == MULCOPY and
-             newtokens[i][1][0] == newtokens[i+1][1][0]):
-
-            offset, value = newtokens[i][1]
-            j = i+1
-            while (j < len(newtokens)-1 and
-                   newtokens[j][0] == MULCOPY and
-                   newtokens[j][1][0] == offset):
-                src, dest, mul = newtokens[j][1]
-                newtokens2.append((ADD, (dest, value*mul)))
-                j += 1
-
-            newtokens2.append((SET, (offset, value)))
-            i = j - 1
-            optimized = True
-
-        if not optimized:
-            newtokens2.append(newtokens[i])
-
-        i += 1
-
     # Optimize recursively
-    if newtokens2 != tokens:
-        return optimize(newtokens2)
+    if newtokens != tokens:
+        return optimize(newtokens)
 
-    return newtokens2
+    return newtokens
