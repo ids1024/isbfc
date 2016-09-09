9e6dfb85003f42e8ee530ab8a33f93645a29700f
Ian Douglas Scott <ian@iandouglasscott.com>
2016-09-09 09:23:12 -0700

    Document Token

--- src/token.rs
+++ src/token.rs
@@ -1,17 +1,29 @@
 use std::fmt;
 
+/// A Token in isbfc's intermediate representation.
 #[derive(Clone, PartialEq, Eq)]
 pub enum Token {
+    /// `Output` Writes output buffer to stdout
     Output,
+    /// `Input` Reads one byte from stdin to the current cell
     Input,
+    /// `Loop(content)` Runs *content* in loop while current cell is not zero
     Loop(Vec<Token>),
+    /// `Move(offset)` Moves data pointer by *offset* cells
     Move(i32),
+    /// `Add(offset, value)` Adds *value* to cell at *offset*
     Add(i32, i32),
+    /// `Set(offset, value)` Sets cell at *offset* to *value*
     Set(i32, i32),
+    /// `MulCopy(src, dest, mul)` Adds product of *mul* and the value at offset *src* to the cell at offset *dest*
     MulCopy(i32, i32, i32),
+    /// `Scan(offset)` Equivalent to `Loop(Move(offset))`
     Scan(i32),
+    /// `LoadOut(offset, add)` Appends the value of the cell at *offset* plus *add* to the output buffer
     LoadOut(i32, i32),
+    /// `LoadOutSet(value)` Appends the constant value *value* to the output buffer
     LoadOutSet(i32),
+    /// `LoadOutSet(offset, content)` Runs *content* if the cell at *offset* is not zero
     If(i32, Vec<Token>),
 }
 
