e83791b3b953a0273f94fd7f896659a1397d4bd8
Ian Douglas Scott <ian@iandouglasscott.com>
2026-02-15 15:50:02 -0800

    bin/codegen_c: Use `NewOptimizer`

--- src/bin/codegen_c.rs
+++ src/bin/codegen_c.rs
@@ -1,5 +1,5 @@
 use isbfc::codegen::c_codegen::{codegen, CellType};
-use isbfc::{OldOptimizer, Optimizer};
+use isbfc::{NewOptimizer, Optimizer};
 use std::io::{Read, Write};
 
 fn main() {
@@ -7,7 +7,7 @@ fn main() {
     std::io::stdin().read_to_end(&mut code).unwrap();
 
     let ast = isbfc::parse(&code).unwrap();
-    let lir = OldOptimizer.optimize(&ast, 3);
+    let lir = NewOptimizer.optimize(&ast, 3);
     let c = codegen(&lir, CellType::U64, 8192);
 
     std::io::stdout().write_all(c.as_bytes()).unwrap();
